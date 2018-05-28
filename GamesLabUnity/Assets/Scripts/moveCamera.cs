3a5429c34e381bab23d3cc09dce1aa9e6642b9f5
Patrick <patrick.haertl@tum.de>
2018-05-28 12:01:19 +0200

    transform Camera

--- GamesLabUnity/Assets/Scripts/moveCamera.cs
+++ GamesLabUnity/Assets/Scripts/moveCamera.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 
 public class moveCamera : MonoBehaviour {
 
+
+    public bool shadow = false;
+    public bool finished = true;
+    RaycastHit hit;
+    float counter = 52f;
+    int speed = 80;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,11 +19,36 @@ public class moveCamera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-	 var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+	/* var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        transform.Translate(0, 0, z);*/
 		
+
+        if (shadow && !finished)
+        {
+
+
+                // this.gameObject.transform.Rotate(new Vector3(1.0f,0,0), Time.deltaTime,hit.point);
+                transform.RotateAround(hit.point, new Vector3(1.0f, 0, 0), Time.deltaTime*speed);
+            counter = counter + Time.deltaTime * speed;
+
+        }
+        
+        if (counter >= 90)
+        {
+            finished = true;
+        }
 	}
+
+    public void changePlane()
+    {
+        Physics.Raycast(transform.position, transform.forward, out hit);
+        shadow = !shadow;
+
+        finished = false;
+
+
+    }
 }
