e29910af3fe354b27d26002c33ac4df1bdc0ded7
unknown <tipecska@in.tum.de>
2018-05-28 11:18:10 +0200

    started puzzles and scene changes

---
+++ GamesLabUnity/Assets/ControllParticleSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllParticleSystem : MonoBehaviour {
+
+
+	void OnAnwake(){
+
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
