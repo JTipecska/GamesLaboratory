db547592e9b624695be219f54ff96c2d6652da72
PatrickH <haertl@tum.de>
2018-05-30 14:35:44 +0200

    Added Ingame Menu/Trigger

---
+++ GamesLabUnity/Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+    public GameObject key;
+    public GameObject message;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        key.gameObject.SetActive(true);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        key.transform.LookAt(Camera.main.transform.position, -Vector3.up);
+        if (Input.GetKeyDown("Inspect"))
+        {
+            message.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        key.gameObject.SetActive(false);
+    }
+}
