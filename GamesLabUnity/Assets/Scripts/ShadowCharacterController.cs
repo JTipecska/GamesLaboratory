3f22a2e8055b9e2c03fc5e97cff8201a7ba3517f
unknown <tipecska@in.tum.de>
2018-05-31 12:23:49 +0200

    doors in shadow world

--- GamesLabUnity/Assets/Scripts/ShadowCharacterController.cs
+++ GamesLabUnity/Assets/Scripts/ShadowCharacterController.cs
@@ -17,6 +17,7 @@ public class ShadowCharacterController : MonoBehaviour {
             transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
 
         GetComponent<Rigidbody>().MovePosition(transform.position + Vector3.right * Input.GetAxis("CharacterHorizontal") * Time.deltaTime * Data.shadowSpeed);
+        //transform.Translate( Vector3.right * Input.GetAxis("CharacterHorizontal") * Time.deltaTime * Data.shadowSpeed);
         Data.cam.transform.position = new Vector3(transform.position.x, Data.cam.transform.position.y, Data.cam.transform.position.z);
         if (Input.GetButtonDown("Switch World") && Data.cam.GetComponent<TransformCamera>().blendfinished && Data.cam.GetComponent<TransformCamera>().finished && Data.lastWorldSwitch + Data.waitWorldSwitch < Time.time)
         {
