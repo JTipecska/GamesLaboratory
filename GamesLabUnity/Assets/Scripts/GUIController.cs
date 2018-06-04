0c92c0b5ea551e09fa039d3411b7f206e6e12315
unknown <tipecska@in.tum.de>
2018-06-04 13:14:18 +0200

    menu first selected works

--- GamesLabUnity/Assets/Scripts/GUIController.cs
+++ GamesLabUnity/Assets/Scripts/GUIController.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GUIController : MonoBehaviour {
 
-    public GameObject IngameMenu;
+    public GameObject IngameMenu, Eventsystem;
 
     private static bool menuActive = false;
 
     private void Update()
     {
         if (Input.GetKeyDown("escape") || Input.GetButtonDown("Start")) {
-            Action();
+            SetMenuActive();
         }
     }
 
-    public void Action()
+    public void SetMenuActive()
     {
+        Eventsystem.GetComponent<EventSystem>().SetSelectedGameObject(null);
         IngameMenu.SetActive(!IngameMenu.gameObject.activeSelf);
+        Eventsystem.GetComponent<EventSystem>().SetSelectedGameObject(IngameMenu.transform.GetChild(1).gameObject);//.GetComponent<Button>().Select();
+
         menuActive = !menuActive;
 
     }
