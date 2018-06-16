c3bf0cd22d08c0250cdf5a4c7897b27b0b0d9dc3
unknown <tipecska@in.tum.de>
2018-06-16 23:10:53 +0200

    start new scene

--- GamesLabUnity/Assets/Scripts/RealCharacterController.cs
+++ GamesLabUnity/Assets/Scripts/RealCharacterController.cs
@@ -33,7 +33,7 @@ public class RealCharacterController : MonoBehaviour {
                  transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
 
             GetComponent<Rigidbody>().MovePosition(transform.position + Vector3.right * Input.GetAxis("CharacterHorizontal") * Time.deltaTime * Data.speed);
-            Data.shadowCharacter.transform.position = new Vector3(transform.position.x, 0.16f, transform.position.y - 1.4f);
+            Data.shadowCharacter.transform.position = new Vector3(transform.position.x, 0.16f, transform.position.y - 1.3f);
             Data.cam.transform.position = new Vector3(transform.position.x, Data.cam.transform.position.y, Data.cam.transform.position.z);
 
             if (currentLight)
