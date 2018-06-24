01f707e2e38e435d306537182c28158e19a7c12c
PatrickH <haertl@tum.de>
2018-06-25 01:52:33 +0200

    Added door anim Added storytelling

--- GamesLabUnity/Assets/Scripts/TriggerScript.cs
+++ GamesLabUnity/Assets/Scripts/TriggerScript.cs
@@ -7,11 +7,13 @@ public class TriggerScript : MonoBehaviour {
     public GameObject key;
     public GameObject message;
     public bool needPress;
+    public bool follow;
     public GameObject door;
 
 	// Use this for initialization
 	void Start () {
-		
+        key.SetActive(false);
+        message.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -30,14 +32,17 @@ public class TriggerScript : MonoBehaviour {
     private void OnTriggerStay(Collider other)
     {
         key.transform.parent.forward = Camera.main.transform.forward;
+        if (follow)
+            key.transform.position = new Vector3(Data.shadowCharacter.transform.position.x, key.transform.parent.position.y, key.transform.parent.position.z);
         if (Input.GetButtonDown("Inquire") && needPress)
         {
             message.SetActive(true);
             key.SetActive(false);
         }
-        if(Input.GetButtonDown("Action") && door!=null)
+        if(Input.GetButtonDown("Action") && door!=null && other.transform.root == Data.realCharacter.transform.root)
         {
-            door.GetComponent<Animator>().SetBool(0, !door.GetComponent<Animator>().GetBool(0));
+            door.GetComponent<Animator>().SetBool("character_nearby", !door.GetComponent<Animator>().GetBool("character_nearby"));
+            
         }
         
     }
