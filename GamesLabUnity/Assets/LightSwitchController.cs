781a28c8b48a5503481a6c0bfe43442cae7ed9cf
unknown <tipecska@in.tum.de>
2018-06-28 10:48:58 +0200

    gameplay fixes

---
+++ GamesLabUnity/Assets/LightSwitchController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchController : MonoBehaviour {
+
+    public GameObject lightswitch1, lightswitch2, light1, light2;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!lightswitch1.transform.GetChild(0).gameObject.activeSelf)
+        {
+            lightswitch2.transform.GetChild(0).gameObject.SetActive(false);
+            lightswitch2.transform.GetChild(1).gameObject.SetActive(false);
+
+        }
+
+        if (!lightswitch2.transform.GetChild(0).gameObject.activeSelf)
+        {
+            lightswitch1.transform.GetChild(0).gameObject.SetActive(false);
+            lightswitch1.transform.GetChild(1).gameObject.SetActive(false);
+
+        }
+
+
+    }
+}
--- GamesLabUnity/Assets/Scripts/LightSwitch.cs
+++ GamesLabUnity/Assets/Scripts/LightSwitch.cs
@@ -18,14 +18,30 @@ public class LightSwitch : MonoBehaviour {
     void Action()
     {
         // Toggle Billboard_ON and Billboard_OFF
-        foreach(Transform child in transform)
-        {
-            if (child.name.StartsWith("Billboard_O"))
-                child.gameObject.SetActive(!child.gameObject.activeSelf);
+
+        if (lights.Count == 2) {
+
+           // if (lights[0].GetComponent<Light>().enabled == false)
+           // {
+
+                lights[0].SendMessage("Action", SendMessageOptions.DontRequireReceiver);
+                lights[1].SendMessage("Action", SendMessageOptions.DontRequireReceiver);
+           // }
+
         }
 
-        // Toggle every light attached to this object
-        foreach (GameObject g in lights)
-            g.SendMessage("Action", SendMessageOptions.DontRequireReceiver);
+
+        else
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.name.StartsWith("Billboard_O"))
+                    child.gameObject.SetActive(!child.gameObject.activeSelf);
+            }
+
+            // Toggle every light attached to this object
+            foreach (GameObject g in lights)
+                g.SendMessage("Action", SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
