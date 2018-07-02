02216af84bf739e4dad6aa25e1ea7bff963a36fa
unknown <tipecska@in.tum.de>
2018-07-02 13:06:40 +0200

    menu fixes

--- GamesLabUnity/Assets/Scripts/GUIController.cs
+++ GamesLabUnity/Assets/Scripts/GUIController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class GUIController : MonoBehaviour {
+
+    static int levels = 1;
 
     public GameObject IngameMenu, Eventsystem;
 
     private static bool menuActive = false;
+
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("level"))
+            levels = PlayerPrefs.GetInt("level");
+    }
 
     private void Update()
     {
@@ -24,9 +34,44 @@ public class GUIController : MonoBehaviour {
         Eventsystem.GetComponent<EventSystem>().SetSelectedGameObject(IngameMenu.transform.Find("Main/Play").gameObject);
 
         menuActive = !menuActive;
+
+
+        if (!menuActive) {
+
+            IngameMenu.transform.Find("Main").gameObject.SetActive(true);
+            IngameMenu.transform.Find("Image").gameObject.SetActive(true);
+
+            if (IngameMenu.transform.Find("ControlsPicture") != null)
+                IngameMenu.transform.Find("ControlsPicture").gameObject.SetActive(false);
+            if (IngameMenu.transform.Find("OptionsMenu") != null)
+                IngameMenu.transform.Find("OptionsMenu").gameObject.SetActive(false);
+            if (IngameMenu.transform.Find("LevelSelection") != null)
+                IngameMenu.transform.Find("LevelSelection").gameObject.SetActive(false);
+
+        }
     }
 
     public static bool GetMenuActive() {
         return menuActive;
     }
+
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    public void sceneChange(string name)
+    {
+        if (name.Equals("MainMenu") && Data.shadow)
+            Data.cam.GetComponent<TransformCamera>().changePlane();
+        if (name.Equals("cutscene00") && levels == 1)
+        {
+            levels++;
+            PlayerPrefs.SetInt("level", levels);
+        }
+
+        SceneManager.LoadScene(name);
+    }
+
 }
