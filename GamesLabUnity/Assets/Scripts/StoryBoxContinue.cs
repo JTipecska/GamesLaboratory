2713db2e6b4058354e5d9a8ab142906cd80634d3
PatrickH <haertl@tum.de>
2018-07-08 00:48:40 +0200

    Added story again

--- GamesLabUnity/Assets/Scripts/StoryBoxContinue.cs
+++ GamesLabUnity/Assets/Scripts/StoryBoxContinue.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class StoryBoxContinue : MonoBehaviour {
     public static bool storyBoxActive = false;
     public GameObject next;
+    RealCharacterController c;
+    Animator anim;
+    
 	// Use this for initialization
 	void Start () {
-        
+        c = Data.realCharacter.GetComponent<RealCharacterController>();
+        anim = c.GetComponentInChildren<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Data.realCharacter.GetComponent<Rigidbody>().velocity = Vector3.zero;
         storyBoxActive = true;
+        c.animShadow.SetTrigger("StopWalking");
+        c.animShadow.ResetTrigger("StartWalking");
+        c.animShadow.SetBool("Running", false);
+        anim.SetTrigger("StopWalking");
+        anim.ResetTrigger("StartWalking");
+        anim.SetBool("Running", false);
         if (Input.GetButtonDown("Action"))
         {
             storyBoxActive = false;
