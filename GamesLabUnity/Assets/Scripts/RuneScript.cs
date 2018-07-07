199f818b3ad7f8d615ca7d1b94616a1f8ff98b7e
PatrickH <haertl@tum.de>
2018-07-07 03:25:09 +0200

    Added Puzzle in Level 2 It´s ugly but working

---
+++ GamesLabUnity/Assets/Scripts/RuneScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneScript : MonoBehaviour {
+
+
+    public List<GameObject> lights = new List<GameObject>();
+    // Use this for initialization
+    void Start () {
+        Data.interactableObjects.Add(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void Action()
+    {
+        // Toggle Billboard_ON and Billboard_OFF
+        if (gameObject.transform.Find("activation") != null)
+            gameObject.transform.Find("activation").gameObject.SetActive(true);
+
+        // Toggle every light attached to this object
+        foreach (GameObject g in lights)
+        {
+            g.SendMessage("ToggleLight", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
--- GamesLabUnity/Assets/Scripts/StaticLight.cs
+++ GamesLabUnity/Assets/Scripts/StaticLight.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StaticLight : MonoBehaviour {
 
     private bool isON;
-
+    public float duration = -1;
+    float currentTime;
 	// Use this for initialization
 	void Start () {
 
         isON = GetComponent<Light>().enabled;
-
+        currentTime = duration;
+        if (isON && duration > 0)
+            transform.parent.GetComponentInChildren<Text>(true).gameObject.SetActive(true);
     }
 	
 	void OnEnable(){
@@ -23,12 +27,38 @@ public class StaticLight : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+        if (GetComponent<Light>().enabled && duration > 0)
+        {
+            if (currentTime > 0)
+            {
+                transform.parent.GetComponentInChildren<Text>(true).text = (int)currentTime + "";
+               
+                currentTime -= Time.deltaTime;
+            }
+            else
+            {
+                transform.parent.GetComponentInChildren<Text>(true).gameObject.SetActive(false);
+                GetComponent<Light>().enabled = false;
+            }
+        }
+
+        if (duration >0)
+            transform.parent.GetComponentInChildren<Canvas>().transform.forward = Data.cam.transform.forward;
 
     }
 
     void ToggleLight()
     {
         GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+        if (duration > 0)
+        {
+            transform.parent.GetComponentInChildren<Text>(true).gameObject.SetActive(true);
+            if ((int) currentTime <= 0)
+            {
+                currentTime = duration;
+            }
+                 
+        }
     }
 
     void Reset()
