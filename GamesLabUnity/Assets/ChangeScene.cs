7730695129ad14ac6265f969317e1755f40bfff5
unknown <tipecska@in.tum.de>
2018-07-07 13:20:16 +0200

    finished transition

---
+++ GamesLabUnity/Assets/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("Outdoor");
+        Data.realCharacter.GetComponent<Animator>().SetTrigger("TurnForward");
+        Data.realCharacter.GetComponent<Animator>().ResetTrigger("TurnBack");
+        Data.shadowCharacter.GetComponent<Animator>().SetTrigger("TurnForward");
+        Data.shadowCharacter.GetComponent<Animator>().ResetTrigger("TurnBack");
+    }
+}
