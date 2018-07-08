aebe7d9938128d9f9965c2fb7a5c6a4b31bb7d2c
unknown <tipecska@in.tum.de>
2018-07-08 21:48:38 +0200

    outdoor darker

---
+++ GamesLabUnity/Assets/ChangeScene2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("Final");
+
+    }
+}
