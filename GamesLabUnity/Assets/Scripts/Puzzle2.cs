6af439288b8b865484b7d86ab67f2cec32c9c29f
Juli <->
2018-07-08 16:41:59 +0200

    Missing files

---
+++ GamesLabUnity/Assets/Scripts/Puzzle2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle2 : MonoBehaviour {
+    public GameObject[] Switches;
+    public GameObject[] Lights;
+    int currIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeLights()
+    {
+        currIndex++;
+        foreach(GameObject s in Switches)
+        {
+            LightSwitch ls = s.GetComponent<LightSwitch>();
+            if (ls)
+            {
+                for(int i = 0; i < ls.lights.Count; i++)
+                {
+                    ls.lights[i] = Lights[i + currIndex % ls.lights.Count];
+                }
+            }
+        }
+    }
+}
